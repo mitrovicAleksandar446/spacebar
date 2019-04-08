934ef2fce05d156196fe4e89dc1a3eca29789227
Aleksandar Mitrovic <aleksa446@gmail.com>
2019-04-08 23:25:57 +0200

    Added first logger servive

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,10 +41,14 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     * @param $slug
+     * @param LoggerInterface $logger
+     * @return JsonResponse
      */
-    public function toggleArticleHeart()
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
         // ToDo - actually heart the article !
+        $logger->info("Something logged !!!");
         return new JsonResponse(["hearts" => rand(5, 100)]);
     }
 }
