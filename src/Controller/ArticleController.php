f7158ecf778c98497f6b978dc1f78a3f1d499720
Aleksandar Mitrovic <aleksa446@gmail.com>
2019-07-02 23:33:04 +0200

    Removed comments from controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
-use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -51,15 +50,8 @@ class ArticleController extends AbstractController
 //            throw $this->createNotFoundException(sprintf("Article with slug: %s not found", $slug));
 //        }
 
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
-
         return $this->render('article/show.html.twig', [
-            'article' => $article,
-            'comments' => $comments,
+            'article' => $article
         ]);
     }
 
